329b84d373795eea4a313d27c3f543b0038b66d0
Luiza Prestes <taeckls@gmail.com>
2024-03-24 10:09:50 -0300

    feat: dense ranking (start)

---
+++ algorithms/src/ranking/dense_ranking.rs
@@ -0,0 +1,12 @@
+pub fn dense_ranking(ranked_scores: Vec<i32>, last_player_next_scores: Vec<i32>) -> Vec<i32> {
+    println("There are {} ranked players. list={}", ranked_scores.len(), ranked_scores);
+    
+    
+    for i in 1..last_player_next_scores.len() {
+
+    }
+
+
+    let player_ranking = Vec(1);
+    return player_ranking;
+}
